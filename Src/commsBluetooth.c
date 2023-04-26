e94742c485f7b3a221d4cab92b83ab873860cf95
Wingman <weiminshen99@gmail.com>
2023-04-26 15:44:38 -0700

    for GCC

--- Src/commsBluetooth.c
+++ Src/commsBluetooth.c
@@ -255,7 +255,7 @@ void CheckUSARTBluetoothInput(uint8_t USARTBuffer[])
 void SendBluetoothDevice(uint8_t identifier, int16_t value)
 {
 	int index = 0;
-	char charVal[5];
+	char charVal[7];
 	uint8_t buffer[USART_BLUETOOTH_TX_BYTES];
 	
 	// Send bluetooth frame
